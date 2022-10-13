4001758efdcee0a1939f7683e2d989c07dc110c8
Aryan Singh <90084789+aryansingh2020@users.noreply.github.com>
2022-10-13 20:38:30 +0530

    Update bankProj.cpp

--- bankProj.cpp
+++ bankProj.cpp
@@ -1,167 +1,221 @@
 #include<iostream>
+#include<vector>
 #include<ctime>
 #include<cstdlib>
 #include<string>
 using namespace std;
 
-class account
+class Account
 {
     private:
+    long long accountNumber;
+    int pin;
+    long long contactNumber;
     string name;
-    string dob;
-    long acc_no=0;
-    int pin=0;
-    float balance=0;
-    long mob_no=0;
-    public:
-    void createAccount(string a,string b,int c,int d,int e)
+    string dob;  //date validity checker is pending
+    long balance;
+    public :
+    void createAccount()
     {
-        this->name=a;
-        this->dob=b;
-        this->mob_no=c;
-        this->pin=d;
-        this->balance=e;
+        cout<<string(58,'-')<<endl;
+        cout<<"     Enter your name : ";
+        cin.ignore();
+        getline(cin,name);
+        cout<<"     Enter your mobile number :";
+        cin>>contactNumber;  
+        cout<<"     Enter your DOB (dd/mm/yyyy) : ";
+        cin.ignore();
+        getline(cin,dob);
+        cout<<"     Create your 4 digit pin : ";
+        cin>>pin;
+        cout<<"     Enter amount to deposit : ";
+        cin>>balance;
         srand(time(0));
-        this->acc_no=10000000+rand();
-        cout<<"\n################################################\n";
-        cout<<"  Your Account number is : "<<this->acc_no<<endl;
-        cout<<"  Congratulations,your account has been created.\n";
-        cout<<"\n################################################\n";
+        this->accountNumber=700000000000+rand();
+        cout<<"Congratulations! your account has been created.\n";
+        cout<<"Your account number is :"<<this->accountNumber<<endl;
+        cout<<"Please note the account number for future references.\n";
+        cout<<string(58,'-')<<endl;
+    }
+    bool verifier(long long accountNumber,int pin)
+    {
+        if(accountNumber==this->accountNumber && pin==this->pin)
+        {
+            return(true);
+        }
+        else 
+        {
+            return(false);
+        }
     }
     void showDetails()
     {
-        cout<<"\n##########################################\n";
-        cout<<"     Account number : "<<acc_no<<endl;
+        cout<<string(58,'-')<<endl;
+        cout<<"     Account number : "<<accountNumber<<endl;
         cout<<"     Account holder's name : "<<name<<endl;
-        cout<<"     Mobile number : "<<mob_no<<endl;
+        cout<<"     Contact number : "<<contactNumber<<endl;
         cout<<"     DOB : "<<dob<<endl;
         cout<<"     Balance left : "<<balance<<endl;
-        cout<<"\n##########################################\n";
+        cout<<string(58,'-')<<endl;
     }
-    void withDraw(float value)
+    void withdraw()
     {
-        this->balance=this->balance-value;
-        cout<<"\n######################################################\n";
-        cout<<"    Transaction succesfull.\n   Amount debited : "<<value<<endl;
-        cout<<"    Balance left : "<<this->balance<<endl;
-        cout<<"\n######################################################\n";
+        long tempBalance;
+        cout<<string(58,'-')<<endl;
+        cout<<"Enter the amount to withdraw :";
+        cin>>tempBalance;
+        if(tempBalance>this->balance)
+        {
+            cout<<"Enter amount less than current amount.\n";
+            this->withdraw();
+        }
+        else
+        {
+            this->balance=balance-tempBalance;
+            cout<<"Amount withdrawn :"<<tempBalance<<endl;
+            cout<<"Balance left :"<<this->balance<<endl;
+            cout<<string(58,'-')<<endl;
+        }
     }
-    void deposit(float value)
+    void deposit()
     {
-            this->balance=this->balance+value;
-            cout<<"\n######################################################\n";
-            cout<<"    Transaction successfull.\n   Amount deposited : "<<value<<endl;
-            cout<<"    Balance left : "<<balance<<endl;
-            cout<<"\n######################################################\n";
+        long tempBalance;
+        cout<<string(58,'-')<<endl;
+        cout<<"Enter the amount to deposit :";
+        cin>>tempBalance;
+        this->balance=balance+tempBalance;
+        cout<<"Balance left :"<<this->balance<<endl;
+        cout<<string(58,'-')<<endl;
     }
-    int getAccNumb()
+    void changePin()
     {
-        return(this->acc_no);
+        int tempPin1;
+        int tempPin2;
+        cout<<string(58,'-')<<endl;
+        cout<<"Enter the new 4-digit pin :";
+        cin>>tempPin1;
+        cout<<"Re-enter the new 4-digit pin :";
+        cin>>tempPin2;
+        if(tempPin2==tempPin1)
+        {
+            cout<<"Your pin has been updated.\n";
+            this->pin=tempPin2;
+            cout<<string(58,'-')<<endl;
+        }
+        else
+        {
+            cout<<"Error! enter same pin number in above fields.\n";
+            this->changePin();
+        }
+    }
+    long long getAccountNumber()
+    {
+        return(this->accountNumber);
     }
     int getPin()
     {
-        return(pin);
+        return(this->pin);
     }
 };
+
 int main()
 {
+    long long tempAccountNumber;
+    int tempPin;
+    int size=20;
+    Account*acc=new Account[size];
     int user=0;
-    account*accs=new account[20];
     while(user!=20)
     {
         int choice;
-        cout<<"\n######################################################\n";
-        cout<<"  Maximum account allowed in this bank is 20."<<endl;
-        cout<<"            [1] Create account."<<endl;
-        cout<<"            [2] View account details."<<endl;
-        cout<<"            [3] Withdraw money."<<endl;
-        cout<<"            [4] Deposit money."<<endl;
-        cout<<"         Any other key to exit."<<endl;
-        cout<<"  Press the indicated number : ";
+        cout<<string(58,'#')<<endl;
+        cout<<"      1. Create new account.\n";
+        cout<<"      2. See existing user's account details.\n";
+        cout<<"      3. Deposit money.\n";
+        cout<<"      4. Withdraw money.\n";
+        cout<<"      5. Change PIN.\n";
+        cout<<"      6. Exit\n";
+        cout<<"      Please choose the appropriate option :";
         cin>>choice;
-        cout<<"\n######################################################\n";    
-        string username;
-        int pincode;
-        int temp1=0,temp2=0;
-        float temp3=0;
-        int user_numb;
-        string name;
-        string dob;
-        long mob_no;
-        int pin;
-        float balance;
+        cout<<string(58,'#')<<endl;
         switch(choice)
         {
-            case 1:
-            cout<<"\n######################################################\n";
-            cout<<"     Enter your name : ";
-            cin.ignore();
-            getline(cin,name);
-            cout<<"     Enter your mobile number :";
-            cin>>mob_no;   
-            cout<<"     Enter your DOB (dd/mm/yyyy) : ";
-            cin.ignore();
-            getline(cin,dob);
-            cout<<"     Create your Pin : ";
-            cin>>pin;
-            cout<<"     Enter amount to deposit : ";
-            cin>>balance;
-            cout<<"     Your user number is : "<<user+1<<endl;
-            accs[user].createAccount(name,dob,mob_no,pin,balance);
-            cout<<"\n######################################################\n";
+            case 1:acc[user].createAccount();
             user++;
             break;
-
-            case 2: 
-            cout<<"\n######################################################\n";
-            cout<<"Enter your Account number : ";
-            cin>>temp1;
-            cout<<"Enter your Pin number : ";
-            cin>>temp2;
-            cout<<"\n######################################################\n";
-            for(int i=0;i<(user+1);i++)
+            case 2:
+            cout<<"Enter your account number :";
+            cin>>tempAccountNumber;
+            cout<<"Enter your PIN :";
+            cin>>tempPin;
+            for(int i=0;i<=size;i++)
             {
-                if((temp1==accs[i].getAccNumb()) && (temp2==accs[i].getPin()))
+                if(acc[i].verifier(tempAccountNumber,tempPin))
                 {
-                    accs[i].showDetails();
+                    acc[i].showDetails();
+                    break;
                 }
+                else if(i==size)
+                {
+                    cout<<"Please enter valid details.\n";
+                }              
             }
             break;
-
             case 3:
-            cout<<"Enter your Account number : ";
-            cin>>temp1;
-            cout<<"Enter your Pin number : ";
-            cin>>temp2;
-            for(int i=0;i<20;i++)
+            cout<<"Enter your account number :";
+            cin>>tempAccountNumber;
+            cout<<"Enter your PIN :";
+            cin>>tempPin;
+            for(int i=0;i<=size;i++)
             {
-                if((temp1==accs[i].getAccNumb()) && (temp2==accs[i].getPin()))
+                if(acc[i].verifier(tempAccountNumber,tempPin))
                 {
-                    cout<<"Enter the amount to withdraw : ";
-                    cin>>temp3;
-                    accs[i].withDraw(temp3);
+                    acc[i].deposit();
+                    break;
                 }
+                else if(i==size)
+                {
+                    cout<<"Please enter valid details.\n";
+                }              
             }
             break;
-
             case 4:
-            cout<<"Enter your Account number : ";
-            cin>>temp1;
-            cout<<"Enter your Pin number : ";
-            cin>>temp2;
-            for(int i=0;i<20;i++)
+            cout<<"Enter your account number :";
+            cin>>tempAccountNumber;
+            cout<<"Enter your PIN :";
+            cin>>tempPin;
+            for(int i=0;i<=size;i++)
             {
-                if((temp1==accs[i].getAccNumb() && temp2==accs[i].getPin()))
+                if(acc[i].verifier(tempAccountNumber,tempPin))
                 {
-                    cout<<"Enter the amount to deposit : ";
-                    cin>>temp3;
-                    accs[i].deposit(temp3);
+                    acc[i].withdraw();
+                    break;
                 }
+                else if(i==size)
+                {
+                    cout<<"Please enter valid details.\n";
+                }              
             }
             break;
-
-            default:main();
+            case 5:
+            cout<<"Enter your account number :";
+            cin>>tempAccountNumber;
+            cout<<"Enter your PIN :";
+            cin>>tempPin;
+            for(int i=0;i<=size;i++)
+            {
+                if(acc[i].verifier(tempAccountNumber,tempPin))
+                {
+                    acc[i].changePin();
+                    break;
+                }
+                else if(i==size)
+                {
+                    cout<<"Please enter valid details.\n";
+                }              
+            }
             break;
+            default:exit(1);
         }
     }
 }
